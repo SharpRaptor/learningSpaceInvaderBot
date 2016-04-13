71fd0ee96e9c7b9de79dce41e04fcbc92abc418c
unknown <u1251424@CW501-18.AD.HUD.AC.UK>
2016-04-13 10:01:05 +0100

    Initital Commit

---
+++ agents/TJSLearningAgent.cpp
@@ -0,0 +1,11 @@
+#include "TJSLearningAgent.h"
+
+
+TJSLearningAgent::TJSLearningAgent(OSystem * _osystem, RomSettings * _settings)
+{
+}
+
+
+TJSLearningAgent::~TJSLearningAgent()
+{
+}
---
+++ agents/TJSLearningAgent.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "PlayerAgent.hpp"
+
+class TJSLearningAgent : public PlayerAgent
+{
+public:
+	TJSLearningAgent(OSystem * _osystem, RomSettings * _settings);
+	~TJSLearningAgent();
+
+protected:
+
+};
+
